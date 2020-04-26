7d9e608e4c032c80e2570987e9e32f3de5a7fd7d
sibiraj-s <dev.sibiraj@outlook.com>
2020-04-26 18:27:15 +0530

    docs: update example

--- demo/src/app/data.ts
+++ demo/src/app/data.ts
@@ -1,8 +1,9 @@
 /** a array of messages to be shown as random toast */
 export const jsonArray = [
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt . Ut enim ad minim veniam',
-  'Hello Angular 5!',
+  'Hello Angular!',
   'ngx-notifier',
   'A Simple Notification',
-  'Powered by <b>@sibiraj-s</b>'
+  'ngx-notifier by <b>@sibiraj-s</b>',
+  'Follow me <a href="https://github.com/sibiraj-s" target="_blank">@github/sibiraj-s</a>'
 ];
