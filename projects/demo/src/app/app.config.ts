cf485bdd9499cd07b0d275583165ba0b6aba0179
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2024-05-11 20:05:35 +0530

    build: configure ssr

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations()],
+  providers: [provideAnimations(), provideClientHydration()],
 };
